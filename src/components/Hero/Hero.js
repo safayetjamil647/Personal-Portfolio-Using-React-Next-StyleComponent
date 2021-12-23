6d8c4cf89e6f0d6a145da5b3b89870c74ab2435f
Safayet Jamil <work.jamil4@gmail.com>
2021-12-23 18:58:59 +0600

    Update-Project
    	1:Project data and images updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         Trying to be a good human.Love to develop useful UI with impressive UX, Js, React, Vue with savour of backend [ Django-python ] and cloud [AWS] integration .
         </SectionText>
-        <Button onClick={props.handleClick}>Grow More</Button>
+        <Button onClick={props.handleClick}>Know More</Button>
       </LeftSection>
     </Section>
   </>
